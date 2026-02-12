18452276deb6a524f84a77e64096ff8f1d34aca5
Miguel Angel Solano Diaz <msolano684@unab.edu.co>
2026-02-12 11:56:20 -0500

    php-basico

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,52 @@ Route::get('/Mi-Nombre', function () {
 
 Route::get('/php-basico........', function () {
 
+    $name = "Miguel Solano";
+    $height = 1.72;
 
-    return "MIGUEL ANGEL SOLANO DIAZ";
+    $isLogin = true;
+    $age = 20;
+    echo "<h2> ". $name ." </h2>";
+
+    echo "<br><br><br>************** ESTRUCTURAL DE CONTROL******************<br><br><br>";
+
+    $message = "Soy $name, tengo $age años.";
+
+
+    $message .= $isLogin ? "Estoy loguedado" : "No estoy logueado";
+
+    echo $message;
+
+    echo "<br><br><br>************** ESTRUCTURAL DE DATOS******************<br><br><br>";
+
+    $pc = [
+        "name" => "pc Gamer core9",
+        "price" => 6000,
+        "marca" => "Lenovo"
+    ];
+
+    $teclado = [
+        "name" => "pc Gamer core9",
+        "price" => 200,
+        "marca" => "Asus"
+    ];
+
+    $listaProductos = [$pc , $teclado];
+
+    foreach ($teclado as $item){
+        echo $item['name'] . "<br>";
+    }
+
+
+
+
 });
 
+function valadateAge($age){
+    if($age >= 18){
+        return  " Soy mayor de edad.";
+    }else{
+        return "Soy menor de edad.";
+    }
 
+}
